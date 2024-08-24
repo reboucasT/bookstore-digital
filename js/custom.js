cadf27ff8f633da6f1e9108e08567efbbd87515b
reboucasT <143233137+reboucasT@users.noreply.github.com>
2024-08-23 23:39:32 -0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,53 @@
+(function ($) {
+
+  "use strict";
+
+    // inicio
+    $(window).load(function(){
+      $('.preloader').fadeOut(1000); // set duration in brackets    
+    });
+
+
+    // MENU
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(window).scroll(function() {
+      if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+          } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+          }
+    });
+
+
+    // efeito
+    $.stellar({
+      horizontalScrolling: false,
+    }); 
+
+
+    // slider
+    $('.owl-carousel').owlCarousel({
+      animateOut: 'fadeOut',
+      items: 1,
+      loop: true,
+      autoplayHoverPause: false,
+      autoplay: true,
+      smartSpeed: 1000,
+    });
+
+
+    // funcionalidade
+    $(function() {
+      $('.custom-navbar a').on('click', function(event) {
+        var $anchor = $(this);
+          $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+          }, 1000);
+            event.preventDefault();
+      });
+    });  
+
+})(jQuery);
